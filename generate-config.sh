d271716b46ae1760f15eb846a67811a936cb1537
Marian Gasparovic <marian.gasparovic@canonical.com>
2020-07-21 16:26:03 +0200

    prepare for automation with fce_cloud

--- generate-config.sh
+++ generate-config.sh
@@ -3,8 +3,6 @@
 # $2 production dir
 # $3 outdir
 
-rm -rf $3
-
 set -e
 cp -r $2 $3
 mkdir -p $3/generated/maas
@@ -14,8 +12,6 @@ COPY_FROM_LAB="bucketsconfig.yaml hosts.yaml nodes.yaml"
 for i in $COPY_FROM_LAB ; do
     cp $1/config/$i $3/config/
 done
-
-cp $1/generated/maas/virsh_rsa* $3/generated/maas
 
 # change mtu 9000 to 1500
 sed -i "s/9000/1500/" $3/config/networks.yaml
